af3b570d9441ceef4257ac35f41b7998123f3161
Damasus Okeke <gwill2be@gmail.com>
2025-10-14 14:14:47 +0200

    user class code updated

--- User.cs
+++ User.cs
@@ -1,7 +1,7 @@
 
 /*
 Work to be done
-We need ot have a user class with fields:
+We need to have a user class with fields:
 
 Full name
 Date of birth
@@ -16,7 +16,7 @@ Personal
 Admin
 The main functionality:
 
-To be able to crate a new user with full name, date of birth, email, password.
+To be able to create a new user with full name, date of birth, email, password.
 To check if the user already exists by email and password.
 Show the list of the users by their role.
 Hard code BIG ADMIN.
@@ -30,20 +30,39 @@ Have a menu for login and log out.
 
 
 
-namespace User
+namespace App;
+
+public class User
 {
-    // Class for Users
+    public string FirstName;
+    public string LastName;
+    public int DateOfBirth;
+    public int SocialSecurityNumber;
+    public string Email;
+    public string _password;
+    public string Role;
 
-    public class Users
+    public User(string firstName, string lastName, int dateOfBirth, int socialSecurityNumber, string email, string password, string role)
     {
-        public string Username;
-        public string Password;
-
-        public Users(string username, string password)
-        {
-            Username = username;
-            Password = password;
-        }
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        SocialSecurityNumber = socialSecurityNumber;
+        Email = email;
+        _password = password;
+        Role = role;
     }
+
+    public bool TryLogin(string email, string password)
+    {
+        return email == Email && password == _password;
+    }
+
+    public string ToSaveString()
+    {
+        return $"{ Email } , { _password }";
+    }
+    
 }
+
 
