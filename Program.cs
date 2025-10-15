4abc7601f934e8505fc45a5e1822d55a2bc627a6
Your Name Here <youremail@example.com>
2025-10-15 15:49:11 +0200

    Add location detail in main program

--- Program.cs
+++ Program.cs
@@ -2,13 +2,27 @@
 {
 
     public static List<Location> AllLocations = new List<Location>    // Create a public static List that stores all available locations
-    {
 
+    {
 
         new Location("Halmstad Hospital", Region.Hallands),
         new Location("Varberg Clinic", Region.Hallands),
         new Location("Lund Hospital", Region.Skane),
         new Location("Malmö Clinic", Region.Skane)
     };
+     static void Main(string[] args)
+    {
+        Console.WriteLine("=== List of All Locations ===");
+        Console.WriteLine();
+
+        // Loop through all items in the list and print them
+        foreach (var location in LocationData.AllLocations)
+        {
+           Console.WriteLine(location.Name + " - " + location.BelongsToRegion);
+        }
+
+        Console.WriteLine("\nPress Enter to exit...");
+        Console.ReadLine();
+    }
 }
     
