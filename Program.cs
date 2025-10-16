9015223f5ce74c3fd1feb2eae11515a9c508c092
havietkok-sys <haviet.kok@gmail.com>
2025-10-16 14:46:12 +0200

    Removed comments code in program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,4 @@
 ﻿
-// using App
-
-// Region skane = new Region(RegionEnum.Skane);
-// Region halland = new Region(RegionEnum.Halland);
-
-// halland.AddLocation("Nyhems vårdcentral");
-// halland.AddLocation("Halmstad Akut");
-
-// skane.AddLocation("Lund Vårdcentral");
-// skane.AddLocation("Malmö Sjukhus");
-
-// skane.ShowLocations();
-// halland.ShowLocations();
-
-// Console.WriteLine("\nTryck på valfri tangent för att avsluta...");
-// Console.ReadKey();
-
-
-
-
 using App;
 using System.Diagnostics;
 
