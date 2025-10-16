11e29f94fe44dd67aed6c23ff154030694b13c3f
havietkok-sys <haviet.kok@gmail.com>
2025-10-16 10:57:24 +0200

    Removed all excisive "starter" suggegestion data/code Renamed namespace Regions to App

--- Regions.cs
+++ Regions.cs
@@ -1,66 +1,54 @@
-// To create users and bind them to a specific region, we need a region class.
 
-// Work to be done
-// Create a Region class that will contain fields:
 
-// RegionEnum Name
-// List locations
-// Region enum:
+namespace App
 
-// Skane
-// Halland
-// The thing is, we won't allow the creation of a new Region; it should be hard-coded from the very beginning. BUT, we need to add new locations to the Region.
-
-namespace Regions
-{
   public enum RegionEnum
+{
+  Skane,
+  Halland
+}
+
+public class Location
+{
+  public string Name;
+
+  public Location(string name)
   {
-    Skane,
-    Halland
+    Name = name;
+  }
+}
+
+public class Region
+{
+  public RegionEnum Name;
+  public List<Location> Locations;
+
+  // Konstruktor
+  public Region(RegionEnum name)
+  {
+    Name = name;
+    Locations = new List<Location>();
   }
 
-  public class Location
+  public void AddLocation(string locationName)
   {
-    public string Name;
-
-    public Location(string name)
-    {
-      Name = name;
-    }
+    Location newLocation = new Location(locationName);
+    Locations.Add(newLocation);
+    Console.WriteLine("Location " + locationName + " has been added to the region " + Name + ".");
   }
 
-  public class Region
+  public void ShowLocations()
   {
-    public RegionEnum Name;
-    public List<Location> Locations;
-
-    // Konstruktor
-    public Region(RegionEnum name)
+    Console.WriteLine("Locations in regionen " + Name + ":");
+    if (Locations.Count == 0)
     {
-      Name = name;
-      Locations = new List<Location>();
+      Console.WriteLine("No locations yet");
     }
-
-    public void AddLocation(string locationName)
+    else
     {
-      Location newLocation = new Location(locationName);
-      Locations.Add(newLocation);
-      Console.WriteLine("Location " + locationName + " has been added to the region " + Name + ".");
-    }
-
-    public void ShowLocations()
-    {
-      Console.WriteLine("Locations in regionen " + Name + ":");
-      if (Locations.Count == 0)
+      foreach (Location loc in Locations)
       {
-        Console.WriteLine("No locations yet");
-      }
-      else
-      {
-        foreach (Location loc in Locations)
-        {
-          Console.WriteLine(" - " + loc.Name);
-        }
+        Console.WriteLine(" - " + loc.Name);
       }
     }
   }
