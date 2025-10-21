88f436b17a64f081bac35ccc1d56153b92cf3d91
Azardani <azarce03@gmail.com>
2025-10-21 15:33:07 +0200

    registration system

--- RegistrationRequest.cs
+++ RegistrationRequest.cs
@@ -1,5 +1,23 @@
 namespace App;
 class RegistrationRequest
 {
+    public string Email;
+    public string Region;
+    public string Password;
+    public string ManegeRegistrationRequest;
+    public string Name;
+    public string LastName;
+    public string SocialNumber;
 
+    public RegistrationRequest(string email, string name, string lname, string password, string snumber, string region)
+    {
+        Email = email;
+        Name = name;
+        Password = password;
+        Region = region;
+        LastName = lname;
+        SocialNumber = snumber;
+        
+    }
+    
 }
