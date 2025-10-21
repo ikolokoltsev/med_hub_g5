db43c5641b0d80c3bc0e5570b83915b8e24e602a
havietkok-sys <haviet.kok@gmail.com>
2025-10-21 13:58:19 +0200

    Journal done but overwrite program.cs and user from main. from my test code.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,8 @@
 using System.Diagnostics;
 using static IOUtilsApp.IOUtils;
 
-List<User> users = new List<User>();
-
-users.Add(new User("Loyd", "Lastname", 26, 19992208, "email@gmail.com", "pass",
-    PermissionEnum.MenagePermissions | PermissionEnum.ViewPermissionList));
+List<User> users = new();
+users.Add(new User("Loyd", "Lastname", 26, 19992208, "email@gmail.com", "pass"));
 users.Add(new User("Max", "Lastname", 26, 19992208, "gmail@gmail.com", "pass"));
 users.Add(new User("Lina", "Lastname", 26, 19992208, "lina@gmail.com", "pass"));
 users.Add(new User("Nick", "Lastname", 26, 19992208, "none@gmail.com", "pass"));
@@ -30,8 +28,6 @@ locations.Add(new Location("Varberg Clinic", RegionEnum.Halland.ToString()));
 locations.Add(new Location("Lund Hospital", RegionEnum.Skane.ToString()));
 locations.Add(new Location("Malmö Clinic", RegionEnum.Skane.ToString()));
 
-// Permissions perm = new Permissions("Max", PermissionEnum.AssignToTheRegions | PermissionEnum.AssignToTheRegions | PermissionEnum.AssignToTheRegions);
-
 InitiateRegionWithLocations(regions, locations);
 
 User? active_user = null;
@@ -45,97 +41,104 @@ while (running)
     switch (menu)
     {
         case Menu.None:
-        {
-            if (active_user == null)
             {
-                Console.WriteLine("1. Register");
-                Console.WriteLine("2. Login");
-                Console.WriteLine("3. Quit");
-                Console.Write("Choose an option: ");
-
-                switch (Console.ReadLine())
+                if (active_user == null)
                 {
-                    case "1": menu = Menu.Register; break;
-                    case "2": menu = Menu.Login; break;
-                    case "3": running = false; break;
+                    Console.WriteLine("1. Register");
+                    Console.WriteLine("2. Login");
+                    Console.WriteLine("3. Quit");
+                    Console.Write("Choose an option: ");
+
+                    switch (Console.ReadLine())
+                    {
+                        case "1": menu = Menu.Register; break;
+                        case "2": menu = Menu.Login; break;
+                        case "3": running = false; break;
+                    }
+                }
+                else
+                {
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                        case "Add Location":
+                            Console.Clear();
+                            Console.Write("Chose a region: ");
+
+
+                            break;
+                    }
                 }
             }
-            else
-            {
-                switch (Console.ReadLine())
-                {
-                    case "1":
-                    case "Add Location":
-                        Console.Clear();
-                        Console.Write("Chose a region: ");
-
-
-                        break;
-                }
-            }
-        }
             break;
         case Menu.Login:
-        {
-            Console.Clear();
-            Console.Write("Enter email: ");
-            string? email = Console.ReadLine();
-            Console.Write("Enter password: ");
-            string? password = Console.ReadLine();
-
-            Console.Clear();
-            Debug.Assert(email != null);
-            Debug.Assert(password != null);
-
-            foreach (User user in users)
             {
-                if (user.TryLogin(email, password))
-                {
-                    active_user = user;
-                    break;
-                }
-            }
+                Console.Clear();
+                Console.Write("Enter email: ");
+                string? email = Console.ReadLine();
+                Console.Write("Enter password: ");
+                string? password = Console.ReadLine();
 
-            menu = Menu.None;
-        }
+                Console.Clear();
+                Debug.Assert(email != null);
+                Debug.Assert(password != null);
+
+                foreach (User user in users)
+                {
+                    if (user.TryLogin(email, password))
+                    {
+                        active_user = user;
+                        break;
+                    }
+                }
+
+                menu = Menu.None;
+            }
             break;
 
         case Menu.Register:
-        {
-            Console.Clear();
-            Console.Write("Enter firstname: ");
-            string? firstName = Console.ReadLine();
-            Console.Write("Enter lastname: ");
-            string? lastName = Console.ReadLine();
-            Console.Write("Enter Date of birth: ");
-            int dateOfBirth = int.Parse(Console.ReadLine());
-            Console.Write("Enter Social Security Number: ");
-            int socialSecurityNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter role/status: ");
-            string? role = Console.ReadLine();
-            Console.Write("Enter email: ");
-            string? email = Console.ReadLine();
-            Console.Write("Enter password: ");
-            string? password = Console.ReadLine();
+            {
+                Console.Clear();
+                Console.Write("Enter firstname: ");
+                string? firstName = Console.ReadLine();
+                Console.Write("Enter lastname: ");
+                string? lastName = Console.ReadLine();
+                Console.Write("Enter Date of birth: ");
+                int dateOfBirth = int.Parse(Console.ReadLine());
+                Console.Write("Enter Social Security Number: ");
+                int socialSecurityNumber = int.Parse(Console.ReadLine());
+                Console.Write("Enter role/status: ");
+                string? role = Console.ReadLine();
+                Console.Write("Enter email: ");
+                string? email = Console.ReadLine();
+                Console.Write("Enter password: ");
+                string? password = Console.ReadLine();
 
-            Console.Clear();
-            Debug.Assert(firstName != null);
-            Debug.Assert(lastName != null);
-            Debug.Assert(dateOfBirth != null);
-            Debug.Assert(socialSecurityNumber != null);
-            // Debug.Assert(role != null);
-            Debug.Assert(email != null);
-            Debug.Assert(password != null);
+                Console.Clear();
+                Debug.Assert(firstName != null);
+                Debug.Assert(lastName != null);
+                Debug.Assert(dateOfBirth != null);
+                Debug.Assert(socialSecurityNumber != null);
+                // Debug.Assert(role != null);
+                Debug.Assert(email != null);
+                Debug.Assert(password != null);
 
-            users.Add(new User(firstName, lastName, dateOfBirth, socialSecurityNumber, email, password));
+                users.Add(new User(firstName, lastName, dateOfBirth, socialSecurityNumber, email, password));
 
-            // TODO: add saving file code here
+                // TODO: add saving file code here
 
-            menu = Menu.None;
-        }
+                menu = Menu.None;
+            }
             break;
     }
 }
+
+
+
+
+
+
+
 
 // TODO: Implement location menu
 /*
--- User.cs
+++ User.cs
@@ -1,29 +1,11 @@
 namespace App;
-
-[Flags]
-public enum PermissionEnum
 {
-    None = 0,
-    MenagePermissions = 1 << 0,
-    AssignToTheRegions = 1 << 1,
-    ManegeRegions = 1 << 2,
-    AddLocations = 1 << 3,
-    CreatePersonnel = 1 << 4,
-    ViewPermissionList = 1 << 5,
-    ManegeRegistrationRequest = 1 << 6,
-    ViewAllUsers = 1 << 7,
-    AddNewUser = 1 << 8,
-    ManageLocations = 1 << 9,
-}
-
-// TODO: implement setting the locale role depending on the context. 
 enum RoleEnum
 {
     Patient,
     Personnel,
     Admin,
 }
-
 public class User
 {
     public string FirstName;
@@ -32,11 +14,10 @@ public class User
     public int SocialSecurityNumber;
     public string Email;
     public string _password;
-    private PermissionEnum _permission;
     // public string? UserRole = RoleEnum.Patient.ToString(); // "Admin", "Personnel", "Patient".
 
-    public User(string firstName, string lastName, int dateOfBirth, int socialSecurityNumber, string email,
-        string password, PermissionEnum initialPermission = PermissionEnum.None)
+
+    public User(string firstName, string lastName, int dateOfBirth, int socialSecurityNumber, string email, string password)
     {
         FirstName = firstName;
         LastName = lastName;
@@ -44,33 +25,22 @@ public class User
         SocialSecurityNumber = socialSecurityNumber;
         Email = email;
         _password = password;
-        _permission = initialPermission;
         // UserRole = role;
+
     }
+
 
     public bool TryLogin(string email, string password)
     {
         return email == Email && password == _password;
     }
 
-    // TODO: AddPermission and RemovePermission probably should be moved into the PermissionManager class(?)
-    public void AddPermission(PermissionEnum permission)
-    {
-        _permission |= permission;
-    }
-
-    public void RemovePermission(PermissionEnum permission)
-    {
-        _permission &= ~permission;
-    }
-
-    public bool HasPermission(PermissionEnum permission)
-    {
-        return _permission.HasFlag(permission);
-    }
-
     public string ToSaveString()
     {
-        return $"{FirstName}, {LastName}; {DateOfBirth}; {SocialSecurityNumber}; {Email};{_password}";
-    }
+        return $"{FirstName}, {LastName}; {DateOfBirth}; {SocialSecurityNumber}; {Email};{_password}"}
+        ;
 }
+}
+
+
+
