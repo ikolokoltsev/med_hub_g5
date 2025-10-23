45dbec02b935a7c5dbb1a54dd6cb9e7909d51428
Azardani <azarce03@gmail.com>
2025-10-23 11:33:36 +0200

    Register system

--- RegistrationRequest.cs
+++ RegistrationRequest.cs
@@ -1,23 +1,29 @@
 namespace App;
+
+enum RegistartonstatusEnum
+{
+    pending, // The User awaits a confirmation
+    completed, // The resgitration is approved by admin
+    Denied, // the application for an account has been denied,
+}
+
 class RegistrationRequest
 {
     public string Email;
     public string Region;
     public string Password;
-    public string ManegeRegistrationRequest;
     public string Name;
     public string LastName;
-    public string SocialNumber;
 
-    public RegistrationRequest(string email, string name, string lname, string password, string snumber, string region)
+
+    public RegistrationRequest(string email, string name, string lname, string password, string region)
     {
         Email = email;
         Name = name;
         Password = password;
         Region = region;
         LastName = lname;
-        SocialNumber = snumber;
-        
+
     }
-    
-}
+
+} 
